e465b1e559e9f065a5a8e01745417a68a6a50336
Gsamsaj <s.jimenez@udea.edu.co>
2026-05-01 23:32:45 -0500

    Modify main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from core.simulation_controller import SimulationController
 from core.visualizer import Visualizer
 
 m1, m2, m3 = 1.0, 1.0, 1.0
-G = 1.0
+G = 1
 
 def main():
     B1 = Body(
@@ -18,22 +18,18 @@ def main():
         mass=m2)
     
     B3 = Body(
-        position=np.array([0.0, 0.0, 0.0]), 
+        position=np.array([0.0, 0.0, 0.5]), 
         velocity=np.array([-0.93240737, -0.86473146, 0.0]),  
         mass=m3)
-
-    impulse0 = Propulsion(tf=5, dVx=-1.0, dVy=-1.0, dVz=1)
     
     bodies = [B1, B2, B3]
-    impulses = [impulse0]
     
     controller = SimulationController(
         bodies=bodies,
         G=G,
         ti=0,
-        tf=20,
-        fps=120,
-        impulse=impulses)
+        tf=22,
+        fps=120)
 
     orbits = controller.run_solution()
     
@@ -41,7 +37,7 @@ def main():
         bodies=bodies,
         trajectories=orbits,
         follow=np.inf,
-        speed=10)
+        speed=12)
 
     viz.animate()
     
