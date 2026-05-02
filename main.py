6ca254468d4b337aa13613f698cc5fa6b6f534a8
Gsamsaj <s.jimenez@udea.edu.co>
2026-05-01 21:39:28 -0500

    Modify main module for testing

--- main.py
+++ main.py
@@ -2,40 +2,37 @@ import numpy as np
 from core.entities import Body, Propulsion
 from core.simulation_controller import SimulationController
 from core.visualizer import Visualizer
-import mission_design.hohmann as hm
 
-G = 6.6743e-20          # km^3 / kg / s^2
-earth_mass = 5.972e24   # kg
-mu = G * earth_mass     # km^3 / s^2
-
-# Orbit Radius
-R_earth = 6371      # km
-r1 = R_earth + 300  # km
-r2 = R_earth + 4000 # km
-
-# Hohmann Transfer Data
-V_r1 = hm.V_orbit(r1, r1, mu)
-dV1, dV2, T_trans = hm.hohmann_transfer(r1, r2, mu)
-T0, T2 = hm.T_imp(r1, r2, mu)  # R1 & R2 Orbit Period
-
-T_tot = T0 + T_trans/2 + T2
+m1, m2, m3 = 1.0, 1.0, 1.0
+G = 1.0
 
 def main():
-    earth = Body(position=np.array([0.0, 0.0]), velocity=np.array([0.0, 0.0]),  mass=earth_mass)
-    sat = Body(  position=np.array([r1, 0.0]),  velocity=np.array([0.0, V_r1]), mass=1) 
-
-    impulse0 = Propulsion(tf=T0,             dVx=dV1, dVy=0.0, dVz=0.0)
-    impulse1 = Propulsion(tf=T0 + T_trans/2, dVx=dV2, dVy=0.0, dVz=0.0)
+    B1 = Body(
+        position=np.array([0.97000436, -0.24308753, 0.0]), 
+        velocity=np.array([0.466203685, 0.43236573, 0.0]),  
+        mass=m1)
     
-    bodies = [earth, sat]
-    impulses = [impulse0, impulse1]
+    B2 = Body(
+        position=np.array([-0.97000436, 0.24308753, 0.0]), 
+        velocity=np.array([0.466203685, 0.43236573, 0.0]),  
+        mass=m2)
+    
+    B3 = Body(
+        position=np.array([0.0, 0.0, 0.0]), 
+        velocity=np.array([-0.93240737, -0.86473146, 0.0]),  
+        mass=m3)
+
+    impulse0 = Propulsion(tf=5, dVx=-1.0, dVy=-1.0, dVz=1)
+    
+    bodies = [B1, B2, B3]
+    impulses = [impulse0]
     
     controller = SimulationController(
         bodies=bodies,
         G=G,
         ti=0,
-        tf=T_tot*1.5,
-        step=20,        # Number of frames per --X-- seconds of simulation.
+        tf=20,
+        fps=120,
         impulse=impulses)
 
     orbits = controller.run_solution()
@@ -44,8 +41,7 @@ def main():
         bodies=bodies,
         trajectories=orbits,
         follow=np.inf,
-        speed=5
-    )
+        speed=10)
 
     viz.animate()
     
