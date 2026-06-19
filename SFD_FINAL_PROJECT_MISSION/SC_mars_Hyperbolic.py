2103acb690a36dc09a268b66d1e05f75d5e3228d
Gsamsaj <s.jimenez@udea.edu.co>
2026-06-19 14:07:03 -0500

    Deleting plot redundancy

--- SFD_FINAL_PROJECT_MISSION/SC_mars_Hyperbolic.py
+++ SFD_FINAL_PROJECT_MISSION/SC_mars_Hyperbolic.py
@@ -24,37 +24,6 @@ dV_moi = np.array([-0.10738625, -2.40302961])
 # Impulse for circularization
 impulse_moi = Propulsion(tf=t_burn, dVx=dV_moi[0], dVy=dV_moi[1], dVz=0.0)
 
-# =========================================================
-# Plots the relative distance between SC and Mars to confirm orbital circularization
-def plot_distance(orbits, ti, tf):
-    
-    pos_mars = orbits[0][0] 
-    pos_sc = orbits[0][1]   
-    
-    # Relative distance vectors
-    rel_vec = pos_sc - pos_mars
-    dist = np.linalg.norm(rel_vec, axis=0)
-    
-    # Time axis in days
-    time_days = np.linspace(ti, tf, len(dist))/86400
-    burn_day = t_burn/86400
-    
-    plt.figure(figsize=(10, 6))
-    plt.plot(time_days, dist, label='Distance Spacecraft - Mars', color='#2ca02c', linewidth=2)
-    plt.axvline(x=burn_day, color='red', linestyle='--', label=f'Burn at Periapsis (Day {burn_day:.2f})')
-    
-    # Circular parking orbit radius
-    final_radius = dist[-1]
-    plt.axhline(y=final_radius, color='blue', linestyle=':', label=f'Final Radius: {final_radius:.2f} km')
-    
-    plt.title('Hyperbolic Capture')
-    plt.xlabel('Time (Days)')
-    plt.ylabel('Distance (km)')
-    plt.grid(True, alpha=0.5)
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-
 def main():
 
     mars = Body(position=np.array([0.0, 0.0]), velocity=np.array([0.0, 0.0]), mass=mars_mass)
@@ -74,7 +43,6 @@ def main():
         impulse=[impulse_moi])
 
     orbits = controller.run_solution()
-    plot_distance(orbits, 0, t_final)
 
     viz = Visualizer(
         bodies=bodies,
